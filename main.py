a01bc3f4e311b1e8eab905bc0b19c963b9a9f6ec
Nigel Campbell <ncampbell7@gatech.edu>
2019-04-19 16:37:02 -0400

    Updated model to include training function. Pushing most recent notebook.

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ architectures for NLP language models.
 parser = argparse.ArgumentParser(description=description)
 parser.add_argument('--data', type=str, required=True, help='location of the data corpus')
 parser.add_argument('--verbose', action='store_true', help='Enables verbose output.')
-parser.add_argument('--emsize', type=str, default=10, help='embedding size')
+parser.add_argument('--emsize', type=int, default=200, help='embedding size')
+parser.add_argument('--nhidden', type=int, default=10, help='number hidden')
+parser.add_argument('--nlayers', type=int, default=2, help='number of layers' )
+parser.add_argument('--epochs', type=int, default=100, help='number of epochs')
 args = parser.parse_args()
 
 path = args.data
 verbose = args.verbose
 embed_size = args.emsize
+nhidden = args.nhidden
+nlayers = args.nlayers
+epochs = args.epochs
 
 corpus = data.Corpus(path, verbose)
 
@@ -23,7 +29,7 @@ vocab_size = len(corpus)
 
 if verbose:
     print('Read {} tokens'.format(vocab_size))
-model = model.Model(vocab_size, embed_size)
+lm = model.Model(vocab_size, embed_size, nhidden, nlayers)
+losses = lm.fit(corpus, epochs)
+print(losses)
 
-print(corpus.data)
-print(model(corpus.data[0]))
--- model.py
+++ model.py
@@ -3,13 +3,29 @@ import torch.nn as nn
 import torch.nn.functional as F
 import data
 
+
+def batch(data, i, length):
+    source = data[i:i+length]
+    target = data[i+1:i+length+1].to(torch.long)
+    return source, target
+
+
 class Model(nn.Module):
         
-    def __init__(self, vocab_size, embed_size, nhidden, nlayers):
+    def __init__(self, vocab_size, embed_size, nhidden, nlayers, model='LSTM'):
         super(Model, self).__init__()
         self.encoder = nn.Embedding(vocab_size, embed_size)
-        self.rnn = nn.RNN(embed_size, nhidden, nlayers)
+        if model == 'LSTM':
+            self.rnn = nn.LSTM(embed_size, nhidden, nlayers)
+        else:
+            self.rnn = nn.RNN(embed_size, nhidden, nlayers)
         self.decoder = nn.Linear(nhidden, vocab_size)
+        
+        self.vocab_size = vocab_size
+        self.embed_size = embed_size
+        self.nhidden = nhidden
+        self.nlayers = nlayers
+        self.model = model
 
     def forward(self, x, h0):
         y = self.encoder(x)
@@ -18,70 +34,70 @@ class Model(nn.Module):
         y = self.decoder(y)
         return y, h1
 
-'''
-Converts sequence to tensor.
-'''
-def seq2ix(seq, corpus):
-    sequence = [corpus.word2ix[val] for val in seq.split()]
-    return torch.tensor(sequence)
+    '''
+    Converts sequence to tensor.
+    '''
+    def seq2ix(self, seq, corpus):
+        sequence = [corpus.word2ix[val] for val in seq.split()]
+        return torch.tensor(sequence)
 
-'''
-Converts tensor output to actual words.
-'''
-def ix2word(data, corpus):
-    to_word = lambda val, corpus: corpus.ix2word[val]
-    return map(lambda x: to_word(x, corpus), data)
+    '''
+    Converts tensor output to actual words.
+    '''
+    def ix2word(self, data, corpus):
+        to_word = lambda val, corpus: corpus.ix2word[val]
+        return map(lambda x: to_word(x, corpus), data)
 
+    def init_hidden(self, batch_size):
+        h0 = torch.zeros((self.nlayers, batch_size, self.nhidden))
+        if self.model == 'LSTM':
+            return (h0, h0)
+        return h0
+    
+    '''
+    Runs a single training epoch on the dataset.
 
-def batch(data, i, length):
-    source = data[i:i+length]
-    target = data[i+1:i+length+1].to(torch.long)
-    return source, target
+    Warning: One EPOCH took
+    CPU times: user 2min 45s, sys: 40.5 s, total: 3min 26s
+    Definitely needs to be trained on ICEHAMMER GPUs.
+    '''
+    def _train(self, corpus, seq_length, criterion=nn.CrossEntropyLoss(), lr=0.01, momentum=0.9, start = 0):
+        self.train() # Sets the module in training mode.
+        optimizer = torch.optim.SGD(self.parameters(), lr=lr, momentum=momentum)
+        total_loss = 0.
+        data = corpus.data
+        length = data.size(0)
+        iterations = 1
+        for i in range(start, length-seq_length, seq_length):
+            self.zero_grad()
+            source, targets = batch(data, i, seq_length)
+            source = source.to(torch.long)
+            hidden = self.init_hidden(source.size(0))
+            output, hidden = self(source, hidden)
+            output = output.squeeze()
+            loss = criterion(output, targets)
+            total_loss += loss.item()
+            iterations += 1
+            loss.backward() 
+            optimizer.step()
+        return total_loss / iterations
 
-'''
-Runs a single training epoch on the dataset.
+    def fit(self, corpus, epochs, seq_length = 15, cuda=False):
+        loss_history = []
+        print("Running for {} epochs".format(epochs))
+        for epoch in range(epochs):
+            loss = self._train(corpus, seq_length)
+            print("Loss {}, Epoch {}".format(loss, epoch))
+            loss_history.append(loss)
+        return loss_history
 
-Warning: One EPOCH took
-CPU times: user 2min 45s, sys: 40.5 s, total: 3min 26s
-Definitely needs to be trained on ICEHAMMER GPUs.
-'''
-def train(lm, corpus, seq_length, criterion=nn.CrossEntropyLoss(), lr=0.01, momentum=0.9, start = 0):
-    lm.train()
-    optimizer = torch.optim.SGD(lm.parameters(), lr=lr, momentum=momentum)
-    total_loss = 0.
-    data = corpus.data
-    length = data.size(0)
-    iterations = 1
-    for i in range(start, length-seq_length, seq_length):
-        lm.zero_grad()
-        source, targets = batch(data, i, seq_length)
-        source = source.to(torch.long)
-        h0 = torch.zeros((nlayers, source.size(0), nhidden)) #TODO Come up with better initial hidden
-        output, hidden = lm(source, h0)
-        output = output.squeeze()
-        loss = criterion(output, targets)
-        total_loss += loss.item()
-        iterations += 1
-        loss.backward() 
-        optimizer.step()
-    return total_loss / iterations
-
-def fit(lm, epochs, seq_length = 15):
-    loss_history = []
-    print("Running for {} epochs".format(epochs))
-    for epoch in range(epochs):
-        loss = train(lm, corpus, seq_length)
-        print("Loss {}, Epoch {}".format(loss, epoch))
-        loss_history.append(loss)
-    return loss_history
-
-def generate(lm, words, corpus):
-    sentence = []
-    for word in words.split():
-        x0 = seq2ix(word, corpus)
-        batch_size = x0.size(0)
-        h0 = torch.zeros((nlayers, batch_size, nhidden))
-        x1, h1 = lm(x0, h0)
-        word = torch.multinomial(x1.div(1).exp(), 1).squeeze()
-        sentence.extend(ix2word([word], corpus))
-    return ' '.join(sentence)
+    def generate(self, words, corpus, batch_size, temperature=1):
+        sentence = []
+        for word in words.split():
+            x0 = seq2ix(word, corpus)
+            batch_size = x0.size(0)
+            h0 = torch.zeros((self.nlayers, batch_size, self.nhidden))
+            x1, h1 = self(x0, h0)
+            word = torch.multinomial(x1.div(temperature).exp(), 1).squeeze()
+            sentence.extend(ix2word([word], corpus))
+        return ' '.join(sentence)
