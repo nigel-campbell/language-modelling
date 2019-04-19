ffa9cfda15fc83ffb44f8f44e50a2c59a5c6c37e
Nigel Campbell <ncampbell7@gatech.edu>
2019-04-19 17:04:57 -0400

    Rearranged args. Added CUDA support.

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ import data
 
 description = """
 Custom driver for evaluating various neural
-architectures for NLP language models.
+architectures for generative language models.
 """
 parser = argparse.ArgumentParser(description=description)
 parser.add_argument('--data', type=str, required=True, help='location of the data corpus')
-parser.add_argument('--verbose', action='store_true', help='Enables verbose output.')
+parser.add_argument('--arch', type=str, default='LSTM', help='Model arch (RNN,LSTM,GRU)')
 parser.add_argument('--emsize', type=int, default=200, help='embedding size')
 parser.add_argument('--nhidden', type=int, default=10, help='number hidden')
 parser.add_argument('--nlayers', type=int, default=2, help='number of layers' )
 parser.add_argument('--epochs', type=int, default=100, help='number of epochs')
+parser.add_argument('--verbose', action='store_true', help='Enables verbose output.')
+parser.add_argument('--cuda', action='store_true', help='Enables cuda')
 args = parser.parse_args()
 
 path = args.data
@@ -22,14 +24,17 @@ embed_size = args.emsize
 nhidden = args.nhidden
 nlayers = args.nlayers
 epochs = args.epochs
+arch = args.arch
+cuda = args.cuda
 
 corpus = data.Corpus(path, verbose)
 
 vocab_size = len(corpus)
 
+device = torch.device("cpu" if not cuda else "cuda")
 if verbose:
     print('Read {} tokens'.format(vocab_size))
-lm = model.Model(vocab_size, embed_size, nhidden, nlayers)
+lm = model.Model(vocab_size, embed_size, nhidden, nlayers, model=model)
+lm = lm.to(device)
 losses = lm.fit(corpus, epochs)
 print(losses)
-
